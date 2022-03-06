ddc3473d069cec95b8b155bb444a66919622ad71
Nubiluma <71071950+Nubiluma@users.noreply.github.com>
2022-03-06 13:26:07 +0100

    added an alternative for disable() method for setting text on button click implemented method to change a view object's background to a random color

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.myfirstapplication;
 
-import static android.graphics.Color.LTGRAY;
+import static android.graphics.Color.rgb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,27 +8,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button b;
+    private Button buttonA;
+    private final Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b = (Button) findViewById(R.id.button);
-        b.setOnClickListener(this::disable);
+
+        buttonA = (Button) findViewById(R.id.button2);
+        buttonA.setOnClickListener(this::disableA);
     }
 
-    public void toggleColor(View view){
-
-        view.setBackgroundColor(LTGRAY);
-    }
-
-    public void disable(View view){
+    //Variant A
+    public void disableA(View view) {
 
         view.setEnabled(false);
-        toggleColor(view);
-        b.setText("Button disabled");
+        buttonA.setText("Button A disabled");
+
+    }
+
+    //Variant B
+    public void disableB(View view) {
+
+        view.setEnabled(false);
+        Button buttonB = (Button) view; //variant 2
+        ((Button) view).setText("Button B disabled");
+    }
+
+    /**
+     * Sets background of view object to a randomly chosen color (rgb value)
+     * @param view
+     */
+    public void toggleRandomColor(View view) {
+
+        view.setBackgroundColor(rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256)));
     }
 }
