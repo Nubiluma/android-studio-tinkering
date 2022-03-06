4148e64a391fee7fbfac8c6d736672ca9579629a
Nubiluma <71071950+Nubiluma@users.noreply.github.com>
2022-03-06 14:26:45 +0100

    implemented editText() method to edit text on view object through user input

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -7,12 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonA;
     private final Random random = new Random();
 
     @Override
@@ -20,14 +21,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonA = (Button) findViewById(R.id.button2);
-        buttonA.setOnClickListener(this::disableA);
     }
 
     //Variant A
     public void disableA(View view) {
 
         view.setEnabled(false);
+        Button buttonA = (Button) findViewById(R.id.button2);
+        buttonA.setOnClickListener(this::disableA);
         buttonA.setText("Button A disabled");
 
     }
@@ -47,5 +48,17 @@ public class MainActivity extends AppCompatActivity {
     public void toggleRandomColor(View view) {
 
         view.setBackgroundColor(rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256)));
+    }
+
+    /**
+     * Lets the user rename textview object through text input
+     * @param view
+     */
+    public void editText(View view){
+        EditText t = findViewById(R.id.nameText);
+        String input = t.getText().toString();
+
+        TextView v = (TextView) findViewById(R.id.textView);
+        v.setText(input);
     }
 }
